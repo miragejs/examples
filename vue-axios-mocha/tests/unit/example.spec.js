d433440bef18bb4e35b0bcc968dd40d9a4badca4
Sam Selikoff <sam.selikoff@gmail.com>
2020-09-30 14:19:24 -0500

    Force node to use monkey patched window.xhr

--- vue-axios-mocha/tests/unit/example.spec.js
+++ vue-axios-mocha/tests/unit/example.spec.js
@@ -2,19 +2,24 @@ import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 import { makeServer } from "../../src/server";
-import axios from "axios";
-import XhrAdapter from "axios/lib/adapters/xhr";
-axios.defaults.adapter = XhrAdapter;
 
 describe("HelloWorld.vue", () => {
   let server;
+  let originalXMLHttpRequest = XMLHttpRequest;
 
   before(() => {
     server = makeServer({ environment: "test" });
+    // Force node to use the monkey patched window.XMLHttpRequest
+    // This needs to come after `makeServer()` is called.
+    // eslint-disable-next-line no-global-assign
+    XMLHttpRequest = window.XMLHttpRequest;
   });
 
   after(() => {
     server.shutdown();
+    // Restore node's original window.XMLHttpRequest.
+    // eslint-disable-next-line no-global-assign
+    XMLHttpRequest = originalXMLHttpRequest;
   });
 
   it("shows users from the server", (done) => {
